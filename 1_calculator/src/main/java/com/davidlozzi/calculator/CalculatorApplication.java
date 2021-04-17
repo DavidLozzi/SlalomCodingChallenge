fa0cb75c7165d35683cb2aa3031967fb727824ce
David Lozzi <lozzi.david@bcg.com>
2021-04-17 18:04:01 -0400

    some error handling, but not there yet

--- 1_calculator/src/main/java/com/davidlozzi/calculator/CalculatorApplication.java
+++ 1_calculator/src/main/java/com/davidlozzi/calculator/CalculatorApplication.java
@@ -2,9 +2,13 @@ package com.davidlozzi.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.script.ScriptException;
@@ -20,13 +24,22 @@ public class CalculatorApplication {
   private final AtomicLong counter = new AtomicLong();
 
   @GetMapping("/calculate")
-  public Answer calculate(@RequestParam(value = "calculation", defaultValue = "") String calculation)
-      throws ScriptException { // why throwing these exceptions?
-    if (calculation != null && calculation != "") {
-      double result = Math.calculate(calculation);
-      return new Answer(counter.incrementAndGet(), result, calculation); // how do i effectively throw a meaningful error back to user?
-    } else {
-      return new Answer(0, 0, "Provide a calculation parameter. Use %2B for +, otherwise, everything else works."); // would like to use a different class for this
+  public ResponseEntity<Answer> calculate(@RequestParam(value = "calculation", defaultValue = "") String calculation)
+      throws ScriptException {
+    try {
+      if (calculation != null && calculation != "") {
+        double result = Math.calculate(calculation);
+        Answer answer = new Answer(counter.incrementAndGet(), result, calculation);
+        return new ResponseEntity<>(answer, HttpStatus.OK);
+      } else {
+        Answer answer = new Answer(0, 0,
+            "make sure to provide your equation in the parameter ?calculation=2*2. use %2B for +");
+        return new ResponseEntity<>(answer, HttpStatus.BAD_REQUEST);
+      }
+    } catch (ScriptException ex) {
+      throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Math error: " + ex.toString());
+    } catch (Exception ex) {
+      throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Error: " + ex.toString());
     }
   }
 }
